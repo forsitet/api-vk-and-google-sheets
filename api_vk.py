c850199234c3201e50f66dcd8cbedaa4e5fe78f8
Denis Budin <D20041152@yandex.ru>
2024-04-11 02:20:37 +0300

    sorting by date and total coverage

--- api_vk.py
+++ api_vk.py
@@ -2,11 +2,12 @@ import requests
 import apiclient
 import httplib2
 from oauth2client.service_account import ServiceAccountCredentials
-import datetime
+from datetime import datetime
 import time
 import os
 from dotenv import load_dotenv
 import schedule
+from date import POST_AFTER_DATE
 
 
 def configurate_google_sheet():
@@ -26,21 +27,31 @@ def post_info(response):
     Фильтрует последние 100 постов на стене по хэштегу #АмбассадорыVK
     """
     try:
+        post_after_date = datetime.strptime(POST_AFTER_DATE, "%d.%m.%Y")
         rows = []
+        rows_sum_views = []
+        sum_view = 0
         cnt = response.json()["response"]["count"]
-        for i in range(cnt):
-            view = response.json()["response"]["items"][i]["views"]["count"]
+        if cnt > 0:
             first_name = response.json()["response"]["profiles"][0]["first_name"]
             last_name = response.json()["response"]["profiles"][0]["last_name"]
-            id_post = response.json()["response"]["items"][i]["id"]
-            id_user = response.json()["response"]["items"][i]["from_id"]
-            url_post = f"https://vk.com/wall{id_user}_{id_post}"
-            time = response.json()["response"]["items"][i]["date"]
-            time = datetime.datetime.fromtimestamp(int(time)).strftime('%d.%m.%Y')
-            rows.append((last_name, first_name, url_post, view, time))
+            for i in range(cnt):
+                response_item = response.json()["response"]["items"][i]
+
+                view = response_item["views"]["count"]
+                id_post = response_item["id"]
+                id_user = response_item["from_id"]
+                url_post = f"https://vk.com/wall{id_user}_{id_post}"
+                post_date = response_item["date"]
+                post_date = datetime.fromtimestamp(int(post_date))
+                if post_date >= post_after_date:
+                    rows.append((last_name, first_name, url_post, view, post_date.strftime('%d.%m.%Y')))
+                    sum_view += int(view)
+            rows_sum_views.append((last_name, first_name, sum_view))
     except:
         print(f"Ошибка у {response.json()["error"]["request_params"][0]["value"]}")
-    return rows
+
+    return rows, rows_sum_views
 
 
 def get_tags():
@@ -53,14 +64,14 @@ def get_tags():
     return tags["values"][0]
 
 
-def send_sheets(data):
+def send_sheets(data, name_table):
     service, spreadsheet_id = configurate_google_sheet()
     values = service.spreadsheets().values().batchUpdate(
         spreadsheetId=spreadsheet_id,
         body={
             "valueInputOption": "USER_ENTERED",
             "data": [
-                {"range": "'Контент'!A:E",
+                {"range": name_table,
                  "majorDimension": "ROWS",
                  "values": [elem for elem in data]},
             ]
@@ -76,17 +87,21 @@ def parser():
     url = "https://api.vk.com/method/" + method_api
     ambs = get_tags()
     rows = [("Фамилия амбассадора", "Имя амбассадора", "Ссылка на пост", "Охват", "Дата поста")]
+    rows_sum_view = [("Фамилия амбассадора", "Имя амбассадора", "Суммарный охват")]
     for domain in ambs:
         time.sleep(1)
         params = {"domain": domain, "v": version, "access_token": token, "query": "#АмбассадорыVK", "owners_only": 1,
                   "count": 100, "extended": 1, "lang": 0}
         response = requests.get(url, params=params)
-        rows.extend(post_info(response))
-    send_sheets(rows)
+        res_info = post_info(response)
+        rows.extend(res_info[0])
+        rows_sum_view.extend(res_info[1])
+    send_sheets(rows, "'Контент'!A:E")
+    send_sheets(rows_sum_view, "'Итог'!A:C")
 
 
 def main():
-    #Убрать комменты, если будем использовать на сервере, это планировщик запуска
+    # Убрать комменты, если будем использовать на сервере, это планировщик запуска
     # schedule.every().day.at("21:00").do(parser)
     # while True:
     #     schedule.run_pending()
