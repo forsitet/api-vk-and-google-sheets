fb094f7f79d1597e7fec48c14b7449446f690679
Denis Budin <D20041152@yandex.ru>
2024-09-08 16:51:13 +0300

    added support for TG channel and VK groups

--- api_vk.py
+++ api_vk.py
@@ -7,8 +7,12 @@ import time
 import os
 from dotenv import load_dotenv
 import schedule
-from data import POST_AFTER_DATE, NAME_TABLE_CONTENT, NAME_TABLE_SUM_VIEW
+from telethon.sync import TelegramClient
+from telethon import functions, types
+from data import POST_AFTER_DATE, NAME_TABLE_CONTENT, NAME_TABLE_SUM_VIEW, TAG
+from collections import namedtuple
 
+Ambs = namedtuple("Ambs", ["second_name", "first_name", "vk", "tg", "vk_group"])
 
 def configurate_google_sheet():
     CREDENTIALS_FILE = "key.json"
@@ -22,7 +26,7 @@ def configurate_google_sheet():
     return service, spreadsheet_id
 
 
-def post_info(response, domain):
+def post_info_vk(response, amb):
     """
     Фильтрует последние 100 постов на стене по хэштегу #АмбассадорыVK
     """
@@ -34,16 +38,9 @@ def post_info(response, domain):
         try:
             cnt = response.json()["response"]["count"]
         except:
-            print(f"У {response.json()["error"]["request_params"][0]["value"]} нет постов")
+            print(f"У {amb.second_name} {amb.first_name} нет постов (cnt=0)")
         else:
-            if cnt > 0:
-                first_name = response.json()["response"]["profiles"][0]["first_name"]
-                last_name = response.json()["response"]["profiles"][0]["last_name"]
-                if not (bool(first_name) and bool(last_name)):
-                    # При репосте из группы не всегда однозначно заносятся поля
-                    first_name = response.json()["response"]["profiles"][-1]["first_name"]
-                    last_name = response.json()["response"]["profiles"][-1]["last_name"]
-
+            if cnt > 0 and cnt:
                 for i in range(cnt):
                     response_item = response.json()["response"]["items"][i]
                     view = 0
@@ -60,26 +57,71 @@ def post_info(response, domain):
                     post_date = response_item["date"]
                     post_date = datetime.fromtimestamp(int(post_date))
                     if post_date >= post_after_date:
-                        rows.append((last_name, first_name, url_post, view, post_date.strftime('%d.%m.%Y')))
+                        rows.append((amb.second_name, amb.first_name, url_post, view, post_date.strftime('%d.%m.%Y')))
                         sum_view += int(view)
-                rows_sum_views.append((last_name, first_name, sum_view))
+                if sum_view > 0:
+                    rows_sum_views.append([amb.second_name, amb.first_name, sum_view])
             else:
-                 print(f"У {domain} нет постов")
+                 print(f"У {amb.second_name} {amb.first_name} нет постов")
     except Exception as err:
-        print(f"Ошибка у {response.json()["error"]["request_params"][0]["value"]} {err}")
+        print(f"Ошибка у {response.json()["error"]["request_params"][0]["value"]} [post_info_vk] {err}")
 
+    if not(rows_sum_views):
+        rows_sum_views.append([amb.second_name, amb.first_name, 0])
     return rows, rows_sum_views
 
 
-def get_tags():
+def post_info_tg(channel_username, amb):
+    api_id = os.getenv("API_ID_TG")
+    api_hash = os.getenv("API_HASH_TG")
+    rows = []
+    sum_view = 0
+    post_after_date = datetime.strptime(POST_AFTER_DATE, "%d.%m.%Y")
+    try:
+        with TelegramClient('session_name', api_id, api_hash) as client:
+            channel = client.get_entity(channel_username)
+            history = client(functions.messages.GetHistoryRequest(
+                peer=channel,
+                limit=400,
+                offset_date=None,
+                offset_id=0,
+                max_id=0,
+                min_id=0,
+                add_offset=0,
+                hash=0,
+            ))
+    except:
+        print(f"У {amb.second_name} {amb.first_name} частный тг-канал, охватов нема")
+
+    else:
+        for message in history.messages:
+            if message.message and TAG in message.message:  # Проверяем наличие строки в тексте сообщения
+                message_link = f"https://t.me/{channel_username.replace('@', '')}/{message.id}"
+                post_date = message.date.replace(tzinfo=None)
+                if post_date >= post_after_date:
+                    rows.append((amb.second_name, amb.first_name, message_link, message.views, post_date.strftime('%d.%m.%Y')))
+                    sum_view += int(message.views)
+                
+    return rows, sum_view
+
+
+def get_ambs_info():
+    N = int(os.getenv("COL"))
     service, spreadsheet_id = configurate_google_sheet()
     tags = service.spreadsheets().values().get(
         spreadsheetId=spreadsheet_id,
-        range="'tags'!C:C",
-        majorDimension='COLUMNS'
+        range="'tags'!A:E",
+        majorDimension='ROWS'
     ).execute()
-    return tags["values"][0]
 
+    ambs_info = []
+    for i in range(len(tags["values"])):
+        amb = tags["values"][i]
+        while len(amb) != N:
+            amb.append("")
+        ambs_info.append(Ambs(*(map(lambda x: x.strip(), amb))))
+    return ambs_info
+ 
 
 def del_sheets(name_table):
     service, spreadsheet_id = configurate_google_sheet()
@@ -112,18 +154,31 @@ def parser():
     token = os.getenv("TOKEN")
     method_api = "wall.search"
     url = "https://api.vk.com/method/" + method_api
-    ambs = get_tags()
     rows = [("Фамилия амбассадора", "Имя амбассадора", "Ссылка на пост", "Охват", "Дата поста")]
     rows_sum_view = [("Фамилия амбассадора", "Имя амбассадора", "Суммарный охват")]
-    for domain in ambs:
+    ambs = get_ambs_info()
+    for amb in ambs:
         time.sleep(1)
-        params = {"domain": domain, "v": version, "access_token": token, "query": "#АмбассадорыVK", "owners_only": 1,
+        params = {"owner_id": amb.vk, "v": version, "access_token": token, "query": TAG, "owners_only": 1,
                   "count": 100, "extended": 1, "lang": 0}
         response = requests.get(url, params=params)
-        res_info = post_info(response, domain)
-        rows.extend(res_info[0])
-        rows_sum_view.extend(res_info[1])
+        res_info_vk = post_info_vk(response, amb)
+        rows.extend(res_info_vk[0])
 
+        if amb.tg != "":
+            res_info_tg = post_info_tg(amb.tg.replace("https://t.me/", ""), amb)
+            res_info_vk[1][0][2] += res_info_tg[1]
+            rows.extend(res_info_tg[0])
+
+        if amb.vk_group != "":
+            params = {"domain": amb.vk_group.replace("https://vk.com/", ""), "v": version, "access_token": token, "query": TAG, "owners_only": 1,
+                  "count": 100, "extended": 1, "lang": 0}
+            response = requests.get(url, params=params)
+            res_info_vk_group = post_info_vk(response, amb)
+            rows.extend(res_info_vk_group[0])
+            res_info_vk[1][0][2] += res_info_vk_group[1][0][2]
+            
+        rows_sum_view.extend(res_info_vk[1])
     del_sheets(name_table_content)
     send_sheets(rows, name_table_content)
     del_sheets(name_table_sum_view)
